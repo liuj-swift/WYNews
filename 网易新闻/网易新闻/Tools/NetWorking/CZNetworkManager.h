f2a9e6e5bfa8c39ef4bc446ddc3d991f435f7267
swift <843818473@qq.com>
2016-06-15 19:32:02 +0800

    准备 新闻接口的网络方法

--- 网易新闻/网易新闻/Tools/NetWorking/CZNetworkManager.h
+++ 网易新闻/网易新闻/Tools/NetWorking/CZNetworkManager.h
@@ -12,4 +12,13 @@
 
 + (instancetype)sharedManager;
 
+/**
+ *  记在网易新闻列表
+ *
+ *  @param channel   频道字符串
+ *  @param start     开始数字
+ *  @param comletion 完成回调[字典数组 / 错误]
+ */
+- (void)newsListWithChannel:(NSString *)channel start:(NSInteger)start completion:(void (^)(NSArray *array,NSError *error))comletion;
+
 @end
