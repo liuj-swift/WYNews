ecb4cb585a22a656f503173cdfdb407038476144
swift <843818473@qq.com>
2016-06-15 17:26:24 +0800

    设置网络工具单例 / 指定 baseURL

---
+++ 网易新闻/网易新闻/Tools/NetWorking/CZNetworkManager.h
@@ -0,0 +1,15 @@
+//
+//  CZNetworkManager.h
+//  网易新闻
+//
+//  Created by swift on 16/6/15.
+//  Copyright © 2016年 swift. All rights reserved.
+//
+
+#import <AFNetworking/AFNetworking.h>
+
+@interface CZNetworkManager : AFHTTPSessionManager
+
++ (instancetype)sharedManager;
+
+@end
---
+++ 网易新闻/网易新闻/Tools/NetWorking/CZNetworkManager.m
@@ -0,0 +1,29 @@
+//
+//  CZNetworkManager.m
+//  网易新闻
+//
+//  Created by swift on 16/6/15.
+//  Copyright © 2016年 swift. All rights reserved.
+//
+
+#import "CZNetworkManager.h"
+
+@implementation CZNetworkManager
+
++ (instancetype)sharedManager {
+    static id instance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        //末尾要有反斜线
+        NSURL *baseURL = [NSURL URLWithString:@"http://c.m.163.com/nc/article/"];
+        
+        instance = [[self alloc] initWithBaseURL:baseURL];
+        
+    });
+    
+    return instance;
+    
+}
+
+@end
