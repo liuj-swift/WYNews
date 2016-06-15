9cfa46e2623739823b9291eb4f193d040e778863
swift <843818473@qq.com>
2016-06-15 16:32:51 +0800

    通过 appearance 设置 tabbar 的外观颜色

--- 网易新闻/网易新闻/AppDelegate.m
+++ 网易新闻/网易新闻/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    [self setupAppearance];
     
     _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     _window.backgroundColor = [UIColor whiteColor];
@@ -30,5 +31,13 @@
     return YES;
 }
 
+/**
+ *  设置外观 appearance 是一个协议,设置控件全局外观
+ */
+- (void)setupAppearance {
+    
+    //设置 tabBar 的渲染颜色 - 会设置'后续[外观设置之后的]' UITabBar 的 tintColor 全部是指定的颜色
+    [UITabBar appearance].tintColor = [UIColor cz_colorWithHex:0xDF0000];
+}
 
 @end
