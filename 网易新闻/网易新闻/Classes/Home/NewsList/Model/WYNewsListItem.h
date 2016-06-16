999e9785e89b61bb056a8e5d6d83849ee09a1aaa
swift <843818473@qq.com>
2016-06-16 08:22:58 +0800

    添加新闻列表数据模型,字典转模型,绑定表格数据

---
+++ 网易新闻/网易新闻/Classes/Home/NewsList/Model/WYNewsListItem.h
@@ -0,0 +1,13 @@
+//
+//  WYNewsListItem.h
+//  网易新闻
+//
+//  Created by swift on 16/6/15.
+//  Copyright © 2016年 swift. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WYNewsListItem : NSObject
+
+@end
---
+++ 网易新闻/网易新闻/Classes/Home/NewsList/Model/WYNewsListItem.m
@@ -0,0 +1,13 @@
+//
+//  WYNewsListItem.m
+//  网易新闻
+//
+//  Created by swift on 16/6/15.
+//  Copyright © 2016年 swift. All rights reserved.
+//
+
+#import "WYNewsListItem.h"
+
+@implementation WYNewsListItem
+
+@end
