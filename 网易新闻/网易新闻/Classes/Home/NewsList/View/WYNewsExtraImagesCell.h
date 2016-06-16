8a9419d436e2db7ea71ce1c1c54a09216befc7e6
swift <843818473@qq.com>
2016-06-16 23:21:43 +0800

    添加多图的cell布局

---
+++ 网易新闻/网易新闻/Classes/Home/NewsList/View/WYNewsExtraImagesCell.h
@@ -0,0 +1,13 @@
+//
+//  WYNewsExtraImagesCell.h
+//  网易新闻
+//
+//  Created by swift on 16/6/16.
+//  Copyright © 2016年 swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYNewsExtraImagesCell : UITableViewCell
+
+@end
