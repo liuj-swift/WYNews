415416d22606a864f8888d2604f8540904217e37
swift <843818473@qq.com>
2016-06-16 08:41:22 +0800

    设置添加普通cell的自定义cell

---
+++ 网易新闻/网易新闻/Classes/Home/NewsList/View/WYNewsNormalCell.h
@@ -0,0 +1,17 @@
+//
+//  WYNewsNormalCell.h
+//  网易新闻
+//
+//  Created by swift on 16/6/16.
+//  Copyright © 2016年 swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYNewsNormalCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *iconView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sourceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *replyLabel;
+
+@end
---
+++ 网易新闻/网易新闻/Classes/Home/NewsList/View/WYNewsNormalCell.m
@@ -0,0 +1,24 @@
+//
+//  WYNewsNormalCell.m
+//  网易新闻
+//
+//  Created by swift on 16/6/16.
+//  Copyright © 2016年 swift. All rights reserved.
+//
+
+#import "WYNewsNormalCell.h"
+
+@implementation WYNewsNormalCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
