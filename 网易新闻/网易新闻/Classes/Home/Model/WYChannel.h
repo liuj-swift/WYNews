c0e6a2ec515b0f2518081d1a0a955a6c219f5624
swift <843818473@qq.com>
2016-06-25 18:33:24 +0800

    添加新闻频道模型

---
+++ 网易新闻/网易新闻/Classes/Home/Model/WYChannel.h
@@ -0,0 +1,21 @@
+//
+//  WYChannel.h
+//  网易新闻
+//
+//  Created by swift on 16/6/25.
+//  Copyright © 2016年 swift. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WYChannel : NSObject
+/**
+ *  频道名称
+ */
+@property (nonatomic,copy) NSString *tname;
+/**
+ *  频道 ID
+ */
+@property (nonatomic,copy) NSString *tid;
+
+@end
---
+++ 网易新闻/网易新闻/Classes/Home/Model/WYChannel.m
@@ -0,0 +1,13 @@
+//
+//  WYChannel.m
+//  网易新闻
+//
+//  Created by swift on 16/6/25.
+//  Copyright © 2016年 swift. All rights reserved.
+//
+
+#import "WYChannel.h"
+
+@implementation WYChannel
+
+@end
