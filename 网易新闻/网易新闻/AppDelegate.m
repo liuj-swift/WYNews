c9163e45ab28062368aca9952bc7d386a8999bd7
swift <843818473@qq.com>
2016-07-31 19:32:06 +0800

    频道视图中的label的布局

--- 网易新闻/网易新闻/AppDelegate.m
+++ 网易新闻/网易新闻/AppDelegate.m
@@ -27,7 +27,7 @@
     _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     _window.backgroundColor = [UIColor whiteColor];
     
-    Class cls = NSClassFromString(@"WYMainViewController");
+    Class cls = NSClassFromString(@"WYHomeViewController");
     UIViewController *vc = [cls new];
     
     _window.rootViewController = vc;
